cb18119936a734183b775e3b4f131a5c426b81fe
Dowon Kang (강도원) <91768420+onekorea37@users.noreply.github.com>
2022-08-27 22:02:56 -0400

    Update #1000.c

--- #1000.c
+++ #1000.c
@@ -1,12 +1,14 @@
-import java.util.Scanner; 
+#include <stdio.h>
 
-class Main {
-		public static void main(String[] args) {
-			Scanner in = new Scanner(System.in);
-			int A=in.nextInt();
-			int B=in.nextInt();
-			
-			System.out.println(A+B);
-			in.close();
-		}
+int main(void) {
+	
+	int A, B;
+	scanf_s("%d %d", &A, &B);
+	printf("%d", A+B); 
+	return 0;
 }
+	/*
+	1. 입력을 어떻게 받는가 -> scanf 
+	2.	출력은 어떻게 하는가-> printf
+	3. %d는 4byte 정수값을 입력받는다.
+	4. scanf 함수에 있는 & 의 의미 : 변수 앞에 & 를 붙히게 되면 해당 변수의 주소를 가리키게 된다. */
